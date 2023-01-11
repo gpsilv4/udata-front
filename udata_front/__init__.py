dfcf51adab92c96630fb1ce71a2fcad71cb14fa0
Gonçalo Silva <goncalo_silv@hotmail.com>
2023-01-11 14:59:35 +0000

    creation pt harvesters and fix pt translations

--- udata_front/__init__.py
+++ udata_front/__init__.py
@@ -1,9 +1,9 @@
 '''
-udata customizations for data.gouv.fr
+udata customizations for dados.gov.pt
 '''
 
 __version__ = '3.0.1.dev'
-__description__ = 'udata customizations for data.gouv.fr'
+__description__ = 'udata customizations for dados.gov.pt'
 
 APIgouvpt_EXTRAS_KEY = 'apigouvpt:apis'
 APIgouvpt_EXPECTED_FIELDS = [
